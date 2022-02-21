1dbe4c3690ab376c393f600c8d1c12215139459e
Ksenia Kuzina <6837643@gmail.com>
2022-02-21 14:44:15 +0100

    rendering data to templates, links to other sections from projects page

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectsList = [
+    {
+        'id': '1',
+        'title': "Ecommerce Website",
+        'description': "Fully functional ecommerce website"
+    },
+    {
+        'id': '2',
+        'title': "Portfolio Website",
+        'description': "Project where I built my own portfolio"
+    },
+    {
+        'id': '3',
+        'title': "Social Network",
+        'description': "Opensource project I'm still working on"
+    },
+]
+
+
 def projects(request):
-    msg = 'You are on the projects page'
-    return render(request, 'projects/projects.html', {'msg':msg})
+    msg = 'projects'
+    number = 11
+    context = {'mag': msg, 'number': number, 'projects': projectsList}
+    return render(request, 'projects/projects.html', context)
+
 
 def project(request, pk):
-    return render(request, 'projects/single_project.html')
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'projects/single_project.html', {'project':projectObj})
